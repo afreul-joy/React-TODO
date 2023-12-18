75d79d9da870d4c5e0e3a7b9fcae89497c4602c6
afreuljoy <afreuljoy@gmail.com>
2023-12-18 20:08:48 +0600

    lot's of changes

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -4,19 +4,7 @@ import { AiOutlineEdit } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
 
 const TaskList = ({ tasks, updateTask, deleteTask }) => {
-  const [editedTask, setEditedTask] = useState("");
   const [strikethroughTasks, setStrikethroughTasks] = useState([]);
-
-  const handleInputChange = (taskId, value) => {
-    setEditedTask({ id: taskId, text: value });
-  };
-
-  const handleUpdateTask = () => {
-    if (editedTask) {
-      updateTask(editedTask.id, editedTask.text);
-      setEditedTask("");
-    }
-  };
 
   const handleStrikeTask = (taskId) => {
     setStrikethroughTasks((prevTasks) => {
@@ -30,57 +18,87 @@ const TaskList = ({ tasks, updateTask, deleteTask }) => {
     });
   };
 
+  const activeTasks = tasks.filter((task) => !strikethroughTasks.includes(task.id));
+  const completedTasks = tasks.filter((task) => strikethroughTasks.includes(task.id));
+
   return (
-    <table className="table-fixed w-full my-2">
-      <thead>
-        <tr>
-          <th className="w-1/8">Strike</th>
-          <th className="w-3/4">Task</th>
-          <th className="w-1/8">Update</th>
-          <th className="w-1/8">Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tasks.map((task) => (
-          <tr  className="text-center" key={task.id}>
-            <td >
-              <input
-                type="checkbox"
-                checked={strikethroughTasks.includes(task.id)}
-                onChange={() => handleStrikeTask(task.id)}
-                className="form-checkbox h-5 w-5 text-blue-500"
-              />
-            </td>
-            <td>
-              <input
-                type="text"
-                value={editedTask.id === task.id ? editedTask.text : task.text}
-                onChange={(e) => handleInputChange(task.id, e.target.value)}
-                className={`py-2 px-4 border rounded focus:outline-none w-full ${
-                  strikethroughTasks.includes(task.id) ? "line-through" : ""
-                }`}
-              />
-            </td>
-            <td>
-              <button
-                onClick={handleUpdateTask}
-                className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 focus:outline-none"
-              >
-                <AiOutlineEdit />
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => deleteTask(task.id)}
-                className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 focus:outline-none"
-              >
-                <MdDelete />
-              </button>
-            </td>
+    <div>
+      <h2 className="text-xl font-bold mb-2">Active Tasks</h2>
+      <table className="table-fixed w-full my-2 border-collapse border border-gray-300">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="w-1/8 p-2 border border-gray-300">Index</th>
+            <th className="w-1/8 p-2 border border-gray-300">Strike</th>
+            <th className="w-3/4 p-2 border border-gray-300">Task</th>
+            <th className="w-1/8 p-2 border border-gray-300">Update</th>
+            <th className="w-1/8 p-2 border border-gray-300">Delete</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {activeTasks.map((task, index) => (
+            <tr className="text-center" key={task.id}>
+              <td className="p-2 border border-gray-300">{index + 1}</td>
+              <td className="p-2 border border-gray-300">
+                <input
+                  type="checkbox"
+                  checked={strikethroughTasks.includes(task.id)}
+                  onChange={() => handleStrikeTask(task.id)}
+                  className="form-checkbox h-5 w-5 text-blue-500"
+                />
+              </td>
+              <td className={`p-2 border border-gray-300 ${strikethroughTasks.includes(task.id) ? "line-through" : ""}`}>
+                {task.text}
+              </td>
+              <td className="p-2 border border-gray-300">
+                <button
+                  onClick={() => updateTask(task.id, task.text)}
+                  className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 focus:outline-none"
+                >
+                  <AiOutlineEdit />
+                </button>
+              </td>
+              <td className="p-2 border border-gray-300">
+                <button
+                  onClick={() => deleteTask(task.id)}
+                  className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 focus:outline-none"
+                >
+                  <MdDelete />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h2 className="text-xl font-bold mb-2">Completed Tasks</h2>
+      <table className="table-fixed w-full my-2 border-collapse border border-gray-300">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="w-1/8 p-2 border border-gray-300">Index</th>
+            <th className="w-3/4 p-2 border border-gray-300">Task</th>
+            <th className="w-1/8 p-2 border border-gray-300">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {completedTasks.map((task, index) => (
+            <tr className="text-center" key={task.id}>
+              <td className="p-2 border border-gray-300">{index + 1}</td>
+              <td className={`p-2 border border-gray-300 ${strikethroughTasks.includes(task.id) ? "line-through" : ""}`}>
+                {task.text}
+              </td>
+              <td className="p-2 border border-gray-300">
+                <button
+                  onClick={() => deleteTask(task.id)}
+                  className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 focus:outline-none"
+                >
+                  <MdDelete />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- src/Pages/Todo/Todo.jsx
+++ src/Pages/Todo/Todo.jsx
@@ -1,33 +1,94 @@
 import React, { useState } from "react";
 import TaskList from "../../Components/TaskList/TaskList";
 import TaskForm from "../../Components/TaskForm/TaskForm";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import UpdateTaskModal from "../../Components/TaskList/UpdateTaskModal";
 
 const Todo = () => {
   const [tasks, setTasks] = useState([]);
+  const [updateTaskData, setUpdateTaskData] = useState(null);
+  const [isUpdateModalOpen, setUpdateModalOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc"); // Default sort order is ascending
 
   const addTask = (task) => {
-    setTasks([task, ...tasks]); // LIFO
-    // setTasks([...tasks, task]); // FIFO
+    setTasks([task, ...tasks]);
+    toast.success("Task updated successfully");
   };
 
   const updateTask = (taskId, updatedTask) => {
+    setUpdateTaskData({ taskId, taskText: updatedTask });
+    setUpdateModalOpen(true);
+  };
+
+  const handleUpdateTask = (updatedTask) => {
     const updatedTasks = [
-      { id: taskId, text: updatedTask },
-      ...tasks.filter((task) => task.id !== taskId),
+      { id: updateTaskData.taskId, text: updatedTask },
+      ...tasks.filter((task) => task.id !== updateTaskData.taskId),
     ];
     setTasks(updatedTasks);
+    setUpdateModalOpen(false);
+    toast.success("Task updated successfully");
   };
 
   const deleteTask = (taskId) => {
     const filteredTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(filteredTasks);
+    toast.error("Task deleted successfully");
   };
+
+  // Filter tasks based on the search term
+  const filteredTasks = tasks.filter((task) =>
+    task.text.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Sort tasks based on task text and sort order
+  const sortedTasks = filteredTasks.sort((a, b) => {
+    const comparison = a.text.localeCompare(b.text);
+    return sortOrder === "asc" ? comparison : -comparison;
+  });
 
   return (
     <div className="container mx-auto mt-8">
       <h1 className="text-3xl font-bold mb-4">Add Your task</h1>
+
+      {/* Search input */}
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search tasks..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="p-2 border border-gray-300 rounded-md"
+        />
+      </div>
+
+      {/* Sort order dropdown */}
+      <div className="mb-4">
+        <label className="mr-2">Sort Order:</label>
+        <select
+          value={sortOrder}
+          onChange={(e) => setSortOrder(e.target.value)}
+          className="p-2 border border-gray-300 rounded-md"
+        >
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </div>
+
       <TaskForm addTask={addTask} />
-      <TaskList tasks={tasks} updateTask={updateTask} deleteTask={deleteTask} />
+      <TaskList tasks={sortedTasks} updateTask={updateTask} deleteTask={deleteTask} />
+
+      {/* Update Task Modal */}
+      {isUpdateModalOpen && (
+        <UpdateTaskModal
+          isOpen={isUpdateModalOpen}
+          onClose={() => setUpdateModalOpen(false)}
+          initialTaskText={updateTaskData.taskText}
+          onUpdate={handleUpdateTask}
+        />
+      )}
     </div>
   );
 };
